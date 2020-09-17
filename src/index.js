38c2ffb6a6b275390a12bfa488ca94d59abd787d
Warren Uhrich <hello@warren.codes>
2020-09-17 10:52:03 -0600

    Add HelloWorld component; add component to index.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <h1>React Git Branch Merge Practice</h1>
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
