dacf7328715f83d7a877ac575ff293be442dd5d9
Warren Uhrich <hello@warren.codes>
2020-09-17 11:11:24 -0600

    Add HelloStudent component; output component in index.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloStudent from './components/HelloStudent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <h1>My Git Test</h1>
+    <HelloStudent name="Eboka" />
+    <HelloStudent name="Atinder" />
   </React.StrictMode>,
   document.getElementById('root')
 );
